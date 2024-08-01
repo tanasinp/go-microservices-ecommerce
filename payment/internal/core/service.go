1ad756fa62b7bf4345c159dd752400104b1edbec
tnsp <wavet888@gmail.com>
2024-08-01 15:39:50 +0700

    Add update status of paymeny and order

--- payment/internal/core/service.go
+++ payment/internal/core/service.go
@@ -1,18 +1,24 @@
 package core
 
+import (
+	adaptersOrder "github.com/tanasinp/go-microservices-ecommerce/payment/internal/adapters"
+)
+
 // primary port
 
 type PaymentService interface {
 	CreatePayment(payment *Payment) error
 	GetPaymentStatusByID(paymentID string) (*Payment, error)
+	UpdatePaymentStatusByID(paymentID string, status string) error
 }
 
 type paymentServiceImpl struct {
-	repo PaymentRepository
+	repo         PaymentRepository
+	orderService adaptersOrder.OrderService
 }
 
-func NewPaymentService(repo PaymentRepository) PaymentService {
-	return &paymentServiceImpl{repo: repo}
+func NewPaymentService(repo PaymentRepository, orderService adaptersOrder.OrderService) PaymentService {
+	return &paymentServiceImpl{repo: repo, orderService: orderService}
 }
 
 func (s *paymentServiceImpl) CreatePayment(payment *Payment) error {
@@ -28,4 +34,18 @@ func (s *paymentServiceImpl) GetPaymentStatusByID(paymentID string) (*Payment, e
 		return nil, err
 	}
 	return payment, nil
+}
+
+func (s *paymentServiceImpl) UpdatePaymentStatusByID(paymentID string, status string) error {
+	if err := s.repo.UpdatePaymentStatusByID(paymentID, status); err != nil {
+		return err
+	}
+	payment, err := s.GetPaymentStatusByID(paymentID) //Fetch payment details to get OrderID
+	if err != nil {
+		return err
+	}
+	if err := s.orderService.UpdateOrderStatus(payment.OrderID, status); err != nil {
+		return err
+	}
+	return nil
 }
